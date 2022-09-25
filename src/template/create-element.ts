602db0d14856c94945aa504557ef70e075503299
Eriri <azunya@126.com>
2022-09-25 23:25:29 +0800

    feat: 模板事件编译

--- src/template/create-element.ts
+++
@@ -1,5 +0,0 @@
-import type { Component } from "./type";
-
-export function createElement(component: Component) {
-  return "";
-}
---
+++ src/template/index.ts
@@ -0,0 +1,16 @@
+import { compiler } from "./compiler";
+import { bindEvent } from "./effect";
+
+import type { Component } from "./type";
+
+export function renderDOM(component: Component, ele: HTMLElement) {
+  // 通过编译模板方式进行渲染
+  const { template, state = {}, methods = {} } = component;
+  const htmlStr = compiler(template, { state, methods });
+
+  // 这里简单用 innerHTML 来做吧
+  ele.innerHTML = htmlStr;
+
+  // 绑定事件
+  bindEvent(methods);
+}
